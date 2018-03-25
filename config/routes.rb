20107e9366e1b22c9f48a73d0dcfac07d890b067
urkazan16 <urkazan16@gmail.com>
2018-03-25 20:50:39 +0400

    Initialize repository

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'users/new'
+
+  root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
