7cf045e32a19f0acebb6840ecf2d84ac96badea6
urkazan16 <urkazan16@gmail.com>
2018-03-28 22:22:20 +0400

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -45,6 +45,11 @@ group :development, :test do
  gem 'rspec-rails', '~> 3.7'
 end
 
+#group :test do
+#  gem 'factory_girl_rails', '4.2.1'
+#end
+
+
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console', '>= 3.3.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, length: { minimum: 6 }
 
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  #validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+  #                  uniqueness: { case_sensitive: false }
+
 end
